e7189b5d1133cb76e76718a71cb85a884bb503e9
koreaioi <koreaioi@gachon.ac.kr>
2025-08-19 04:53:21 +0900

    feat: AdminGuard 추가

---
+++ src/main/java/com/tave/attendance/global/auth/guards/AdminGuard.java
@@ -0,0 +1,17 @@
+package com.tave.attendance.global.auth.guards;
+
+import com.tave.attendance.domain.member.Role;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminGuard extends Guard implements Authorizable {
+
+    public AdminGuard() {
+        super(Role.ADMIN);
+    }
+
+    @Override
+    public boolean isAuthorized(Role role) {
+        return this.role.equals(role);
+    }
+}
