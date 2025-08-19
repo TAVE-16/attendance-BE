2e2eea27e2e704ac25d50d35eaa6c68ccc2bee9a
koreaioi <koreaioi@gachon.ac.kr>
2025-08-19 21:58:49 +0900

    feat: MemberRepository 추가

---
+++ src/main/java/com/tave/attendance/domain/member/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.tave.attendance.domain.member.repository;
+
+import com.tave.attendance.domain.member.entity.Member;
+import com.tave.attendance.domain.member.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmailAndRole(String email, Role role);
+
+}
