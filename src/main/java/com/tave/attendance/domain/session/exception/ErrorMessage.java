52fbf53a456191e26408a975e786ea51831f231e
tjdgus3488 <tjdgus3488@naver.com>
2025-09-05 03:44:17 +0900

    feat: api 호출 시간에 맞춰 세션 조회

--- src/main/java/com/tave/attendance/domain/session/exception/ErrorMessage.java
+++ src/main/java/com/tave/attendance/domain/session/exception/ErrorMessage.java
@@ -10,7 +10,9 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @AllArgsConstructor
 public enum ErrorMessage {
 
-    SESSION_NOT_FOUND(BAD_REQUEST, "존재하지 않는 [세션]입니다.");
+    SESSION_NOT_FOUND(BAD_REQUEST, "존재하지 않는 [세션]입니다."),
+
+    SESSION_NOT_FOUND_BY_DATE(BAD_REQUEST, "해당 날짜에 해당하는 [세션]이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
